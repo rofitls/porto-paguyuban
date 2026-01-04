0e929da0d9ace1c79c766094680cf61afa6da4bd
rofitls <rofituluss2@gmail.com>
2026-01-04 14:39:10 +0700

    revisi tailwind for vercel

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+// import type { Config } from "tailwindcss";
+// const withMT = require("@material-tailwind/react/utils/withMT");
+
+// const config: Config = withMT({
+//   content: [
+//     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+//     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+//   ],
+//   theme: {},
+//   plugins: [],
+// });
+
+// export default config;
+
+const withMT = require("@material-tailwind/react/utils/withMT");
+
+/** @type {import('tailwindcss').Config} */
+module.exports = withMT({
+  content: [
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+});
